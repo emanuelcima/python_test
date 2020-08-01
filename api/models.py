8c17a5101e4cb7367618157e0ed6d2e328b084bc
emanuelcima <emanuel.cima@hotmail.com>
2020-08-01 19:08:13 -0300

    Add field and rain models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from rest_framework import generics
 
-# Create your models here.
+
+class Field(models.Model):
+    """Field Model"""
+    name = models.CharField(max_length=30, primary_key=True)
+    hectares = models.DecimalField(max_digits=10, decimal_places=3)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+
+    def __str__(self):
+        return self.name
+
+
+class Rain(models.Model):
+    """Rain Model"""
+    date = models.DateField()
+    millimeters = models.DecimalField(max_digits=10, decimal_places=3)
+    field = models.ForeignKey(Field, to_field='name', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'field: {self.field}, date: {self.date}, mm: {self.millimeters}'
