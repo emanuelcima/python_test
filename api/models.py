ebed1a5d00ffa706a7c706d125e2c293f16f1533
emanuelcima <emanuel.cima@hotmail.com>
2020-08-02 12:04:56 -0300

    Add max and min for latitude and longitude

--- api/models.py
+++ api/models.py
@@ -1,23 +1,36 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import generics
 
 
 class Field(models.Model):
-    """Field Model"""
+    """
+    Field Model
+    """
     name = models.CharField(max_length=30, primary_key=True)
     hectares = models.DecimalField(max_digits=10, decimal_places=3)
-    latitude = models.DecimalField(max_digits=10, decimal_places=7)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+    longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        validators=[MinValueValidator(-180), MaxValueValidator(180)]
+    )
 
     def __str__(self):
         return self.name
 
 
 class Rain(models.Model):
-    """Rain Model"""
+    """
+    Rain Model
+    """
+    field = models.ForeignKey(Field, to_field='name', on_delete=models.CASCADE)
     date = models.DateField()
     millimeters = models.DecimalField(max_digits=10, decimal_places=3)
-    field = models.ForeignKey(Field, to_field='name', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'field: {self.field}, date: {self.date}, mm: {self.millimeters}'
