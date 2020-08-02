83552bda67e7f14b200a6c48eb5a2461a7b744ef
emanuelcima <emanuel.cima@hotmail.com>
2020-08-02 12:29:31 -0300

    Improve admin

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Field, Rain
 
-admin.site.register((Field, Rain))
+
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hectares', 'latitude', 'longitude')
+    search_fields = ('name',)
+
+class RainAdmin(admin.ModelAdmin):
+    list_display = ('field', 'date', 'millimeters')
+    search_fields = ('field__name', 'date')
+
+
+admin.site.register(Field, FieldAdmin)
+admin.site.register(Rain, RainAdmin)
