858ce6843f287f7bb9503d464f2d04f41f475c64
emanuelcima <emanuel.cima@hotmail.com>
2020-08-02 17:06:44 -0300

    Change SimpleRouter for DefaultRouter

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
 from api.views import FieldViewSet, RainViewSet
 
-router = routers.SimpleRouter()
+router = DefaultRouter()
 router.register(r'fields', FieldViewSet, basename='field')
-router.register(r'rains', RainViewSet)
+router.register(r'rains', RainViewSet, basename='rain')
 
 urlpatterns = router.urls
-urlpatterns = format_suffix_patterns(router.urls)
